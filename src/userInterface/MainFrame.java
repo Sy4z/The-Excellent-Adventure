44bebe63e877d4b7fe92ec065a49d4797650b204
chet00175 <chet00175@yahoo.com>
2014-09-12 07:54:27 +1200

    MainFrame class
    Created the class that represents the main frame of the game in the
    userInterface package.

---
+++ src/userInterface/MainFrame.java
@@ -0,0 +1,60 @@
+package userInterface;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JFrame;
+
+/**
+ * This class represents the main window of the game.
+ * 
+ * @author peesapvenk
+ * 
+ */
+public class MainFrame extends JFrame implements WindowListener {
+	
+	// Stuff to do.
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
