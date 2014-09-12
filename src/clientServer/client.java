2afb33b0f7fde28eae6f78adae1438701754e0a1
Jarred Hone <honejarred@gmail.com>
2014-09-12 15:44:29 +1200

    Minor Additions

--- src/clientServer/client.java
+++ src/clientServer/client.java
@@ -2,6 +2,7 @@ package clientServer;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -19,14 +20,21 @@ public class client {
 	int port = 32768;
 	String addr = "localhost";
 	DataInputStream input;
+	PrintStream output; //Has only partial primitive data support, may want to use dataOutputStream ontop of this (Or instead of)
 
-
+	/**
+	 * Will move most of the calls in the constructor into methods
+	 * @param port Port that the sockets connects through
+	 * @param addr Address that the Socket connects through
+	 */
 	public client(int port, String addr){
 		//this.port = port;
 		//this.addr= addr;
 		try {
 			sock = new Socket(addr, port); //currently connecting at localhost, will need to be changed to server address
-
+			//Maybe check for connection? Possibly might not need to for UDP
+			input = new DataInputStream(sock.getInputStream());
+			output = new PrintStream(sock.getOutputStream());
 		} catch (IOException e) {
 
 			e.printStackTrace();
--- src/clientServer/server.java
+++ src/clientServer/server.java
@@ -1,6 +1,8 @@
 package clientServer;
 
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,14 +17,23 @@ public class server {
 	int port = 32768;
 	ServerSocket sock;
 	Socket clientSock = null;
+	DataInputStream input;
+	PrintStream output;
 	
 	
-	
+	/**
+	 * Will move most of the methods here into something that allows proper connections from clients
+	 */
 	public server(){
 		
 		try {
 			sock = new ServerSocket(port); //Create server socket and broadcast on port
 			clientSock = sock.accept(); //Accept client connection and store in socket clientSock
+			input = new DataInputStream(clientSock.getInputStream());
+			output = new PrintStream(clientSock.getOutputStream());
+			output.close();
+			input.close();
+			clientSock.close();
 		}
 		catch (IOException e) {
 			System.out.println(e);
