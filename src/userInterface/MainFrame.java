61ded5c6ca658af424939eb8160f7324846a8cf7
chet00175 <chet00175@yahoo.com>
2014-09-12 22:00:25 +1200

    Changes and comments to MainFrame

--- src/userInterface/MainFrame.java
+++ src/userInterface/MainFrame.java
@@ -1,7 +1,6 @@
 package userInterface;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.WindowEvent;
@@ -21,42 +20,56 @@ import javax.swing.WindowConstants;
  */
 public class MainFrame extends JFrame implements WindowListener {
 	
+	private static final long serialVersionUID = 1L;
 	private ImageIcon background = new ImageIcon("Crashed_Spaceship.png");
-	
+
 	public MainFrame() {
 		super.addWindowListener(this);
 		this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 		super.setLayout(new BorderLayout());
-		
+
+		JPanel mainPanel = createMainPanel();
+		super.add(mainPanel, BorderLayout.CENTER);
+
+		super.setSize(background.getIconWidth(), background.getIconHeight());
+		super.setLocationRelativeTo(null); // Center the frame.
+		super.setResizable(false);
+		super.setVisible(true);
+	}
+
+	/**
+	 * This method creates the main panel for the frame which displays the main
+	 * menu of the game.
+	 * 
+	 * @return the mainPanel for the game.
+	 */
+	private JPanel createMainPanel() {
 		// Resize the image of the crashed spaceship for display purposes.
-		Image resizedImage = background.getImage().getScaledInstance(1150, 850, Image.SCALE_SMOOTH);
+		Image resizedImage = background.getImage().getScaledInstance(1150, 850,
+				Image.SCALE_SMOOTH);
 		background = new ImageIcon(resizedImage);
-		
-		JPanel mainPanel = new JPanel() {
+
+		JPanel panel = new JPanel() {
+			// Displays the background image on the panel.
 			@Override
 			public void paintComponent(Graphics g) {
 				g.drawImage(background.getImage(), 0, 0, null);
 			}
 		};
-		
-		super.add(mainPanel, BorderLayout.CENTER);
-		
-		super.setSize(background.getIconWidth(), background.getIconHeight());
-		super.setLocationRelativeTo(null); // Center the window.
-		super.setResizable(false);
-		super.setVisible(true);
+
+		return panel;
 	}
 
 	@Override
 	public void windowOpened(WindowEvent e) {
-		// 
+		//
 	}
 
 	@Override
 	public void windowClosing(WindowEvent e) {
-		int confirmed = JOptionPane.showConfirmDialog(null,
-				"Are you sure you want to quit the game?", "",
-				JOptionPane.YES_NO_OPTION);
+		int confirmed = JOptionPane
+				.showConfirmDialog(null, "Are you sure you want to quit?", "",
+						JOptionPane.YES_NO_OPTION);
 		if (confirmed == JOptionPane.YES_OPTION) {
 			dispose();
 			System.exit(0);
@@ -66,35 +79,35 @@ public class MainFrame extends JFrame implements WindowListener {
 	@Override
 	public void windowClosed(WindowEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowIconified(WindowEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowActivated(WindowEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void windowDeactivated(WindowEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		MainFrame frame = new MainFrame();
 	}
-	
+
 }
