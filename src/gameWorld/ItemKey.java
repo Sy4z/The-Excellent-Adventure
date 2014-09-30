4ce4cc1857661121041f7a3cc966ecacc350552e
Chris <chris-mcintosh@hotmail.co.nz>
2014-09-30 14:22:48 +1300

    added Method to take command and empty ItemKey

---
+++ src/gameWorld/ItemKey.java
@@ -0,0 +1,26 @@
+package gameWorld;
+
+import java.awt.Graphics2D;
+
+public class ItemKey extends Item{
+
+	@Override
+	public boolean use() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object[] save() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	void draw(Graphics2D g, int dx, int dy, int dx2, int dy2, int sx, int sy,
+			int sx2, int sy2) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
