76cf030a9c2f8baa635da64634bb129b9450f681
Jarred Hone <honejarred@gmail.com>
2014-10-02 15:10:17 +1300

    Making sure network package is fully updated so Dylan Can start Tests

--- src/main/clientMain.java
+++ src/main/clientMain.java
@@ -1,12 +1,39 @@
 package main;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import clientServer.Client;
+
 public class clientMain {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		runClient("127.0.0.1", 29596);
+
+	}
+	
+	
+	
+	private static void runClient(String addr, int port){
+
+		Socket clientSock = null;
+		try {
+			clientSock = new Socket(InetAddress.getByName(addr), port);
+			Client client = new Client(clientSock);
+			client.start();
+		} catch (UnknownHostException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
 
 	}
 
