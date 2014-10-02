8eee3d4a8e3421533b3323b348f6eb3a77b918fe
Jarred Hone <honejarred@gmail.com>
2014-10-02 14:43:57 +1300

    Modified network package, added code to Main to make server run. Working on Client Currently

--- src/clientServer/Client.java
+++ src/clientServer/Client.java
@@ -18,18 +18,25 @@ import java.net.UnknownHostException;
  * This class can also receive packets back from the server, and perform actions accordingly.
  */
 public class Client extends Thread {
-	Socket sock;
+	private final Socket sock;
 	private DataOutputStream toServer;
 	private DataInputStream fromServer;
 	public Client(Socket socket){
 		this.sock = socket;
+		System.out.println("Client is Constructed");
 	}
 
 
 	public void run(){
+		System.out.println("Trying Connection");
 		try {
 			toServer = new DataOutputStream(sock.getOutputStream());
 			fromServer = new DataInputStream(sock.getInputStream());
+			System.out.println("Client Socket connected on " + sock.getInetAddress() + ":" + sock.getPort());
+			String print = fromServer.readUTF();
+			toServer.writeUTF("Hi Server, From Client");
+			toServer.flush();
+			sock.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import clientServer.Client;
@@ -16,51 +18,82 @@ import clientServer.Server;
 
 public class Main {
 
+	private static int numberOfPlayers = 3; //Variable for the number of players in the game
+	
+	
+	
 	/**
 	 * Main Method to tie the whole program together.
 	 * Using for networking currently.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		runServer("127.0.0.1", 28596);
+		runClient("127.0.0.1", 29596);
 
 	}
-	
-	
+
+
 	/**
 	 * This method controls a Multiplayer game
 	 */
 	private static void multiplayerGame(){
-		
+
 	}
-	
+
 	/**
 	 * This method is in charge of initialising the client
 	 * @param IP Address, Port Number
 	 */
 	private static void runClient(String addr, int port){
-		
-			Socket sock = null;
-			try {
-				sock = new Socket(addr, port);
-			} catch (UnknownHostException e) {
-				
-				e.printStackTrace();
-			} catch (IOException e) {
-				
-				e.printStackTrace();
-			}
-		new clientServer.Client(sock).run();
+
+		Socket clientSock = null;
+		try {
+			clientSock = new Socket(InetAddress.getByName(addr), port);
+			Client client = new Client(clientSock);
+			client.start();
+		} catch (UnknownHostException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
 		}
-		
-	
-	
-	
+
+
+	}
+
+
+
+
 	/**
 	 * This method is in charge of initialising the server
 	 */
-	private static void runServer(){
-		
+	private static void runServer(String addr, int port){
+		ServerSocket serverSock = null;
+		Socket accept = null;
+		try{
+			Server[] serverThreads = new Server[numberOfPlayers];
+			serverSock = new ServerSocket(port);
+			while(true){
+				System.err.println("I didnt accept connection because i suck");
+				accept = serverSock.accept();
+				System.out.println("Accepted Connection from: " + accept.getInetAddress());
+				Server server = new Server(accept);
+				server.start();
+
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		try {
+			serverSock.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
