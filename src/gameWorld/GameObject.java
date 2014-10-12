786a3ae7aaa00a56f4c36b84168c819ea3ce655e
Chris <chris-mcintosh@hotmail.co.nz>
2014-10-12 19:56:14 +1300

    Added getters for server

--- src/gameWorld/GameObject.java
+++ src/gameWorld/GameObject.java
@@ -2,13 +2,18 @@ package gameWorld;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.Serializable;
 
 /**
  *
  * @author ChrisMcIntosh
  *
  */
-public abstract class GameObject {
+public abstract class GameObject implements Serializable{
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 8896684741524936435L;
 	protected BufferedImage img;
 
 	abstract void draw(Graphics2D g, int dx, int dy);
