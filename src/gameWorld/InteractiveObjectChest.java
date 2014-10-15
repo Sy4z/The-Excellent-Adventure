d0f55603fd537ea50926e01224bd3b6e5aaf5c74
Chris <chris-mcintosh@hotmail.co.nz>
2014-10-15 22:25:56 +1300

    Added chest

--- src/gameWorld/InteractiveObjectChest.java
+++ src/gameWorld/InteractiveObjectChest.java
@@ -2,9 +2,14 @@ package gameWorld;
 
 import gameWorld.Inventory.itemTypes;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 /**
  * This is a chest, If a player ends their turn on one of these they will get
@@ -16,6 +21,7 @@ import java.util.ArrayList;
 public class InteractiveObjectChest extends InteractiveObject{
 
 	private int[] contents;
+	private int heightOffSet;
 
 	/**
 	 * Constructs a chest from given contents
@@ -25,6 +31,15 @@ public class InteractiveObjectChest extends InteractiveObject{
 	public InteractiveObjectChest(int[] contents, Point p){
 		super(p);
 		this.contents = contents;
+		try {
+			BufferedImageHolder.addImage(ImageIO.read(new File("src/tile/box.png")),"Player");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		heightOffSet = BufferedImageHolder.getimage("tentacle").getHeight();
+		heightOffSet = Math.max(32 - heightOffSet, heightOffSet -32);
+
 	}
 
 	/**
@@ -37,11 +52,21 @@ public class InteractiveObjectChest extends InteractiveObject{
 		for(int i = 0; i < itms.length; i++)
 			itms[i] = (int) Math.max(0, Math.random()*15 -8);
 		contents = itms;
+		try {
+			BufferedImageHolder.addImage(ImageIO.read(new File("src/tile/box.png")),"Player");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		heightOffSet = BufferedImageHolder.getimage("tentacle").getHeight();
+		heightOffSet = Math.max(32 - heightOffSet, heightOffSet -32);
+
 	}
 
 	@Override
 	public void draw(Graphics2D g, int dx, int dy) {
-		// TODO Auto-generated method stub
+		g.setColor(new Color(155,144,255));
+		g.drawImage(BufferedImageHolder.getimage("box"), dx, dy-heightOffSet, null);
 
 	}
 
