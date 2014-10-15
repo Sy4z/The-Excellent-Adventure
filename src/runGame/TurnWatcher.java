32aca19637a93d835cde1bc48e7e48138abe0d47
Jarred <honejarred@gmail.com>
2014-10-15 17:57:44 +1300

    Minor Network Updates

--- src/runGame/TurnWatcher.java
+++ src/runGame/TurnWatcher.java
@@ -15,22 +15,12 @@ public class TurnWatcher extends Thread{
 	public World world;
 	public static boolean endOfEndPhase = false; //Signifies end of end Phase, so the next turn can switch
 	public static boolean startOfEndPhase = false;
+	public static boolean isEndPhase = false;
 	public TurnWatcher(World world){
 		this.world = world;
 	}
 
 	public void run(){
-	}
-	/**
-	 * Method which controls the gameturns. Starts the turn in gamelogic, and is controlled by network
-	 * @return always returns true, will only return true when the entire turn is finished though.
-	 *
-	 */
-	public boolean turn(){
-		System.err.println("Start of turn");
-		world.startTurn();
-
-		//When this loop breaks, turn is over
 		while(this.isTurn()){
 			System.err.println("In turn loop");
 			try {
@@ -42,11 +32,30 @@ public class TurnWatcher extends Thread{
 			}
 
 			System.err.println("Done sleeping");
+			if(!isTurn()){
+				System.out.println("Turn has ended but is not looping around a last time and is setting variable");
+				isEndPhase = true;
+			}
 		}
+		if(isEndPhase){
 		startOfEndPhase = true;
 		while(!endPhase())//This is just to make the turn stop until the client decides to send the data to the server
 
 		endOfEndPhase = false; //Change to null so that next time turn is called, its going to start the end phase again
+		isEndPhase = false; //Makes this go bback to false
+		}
+		}
+	/**
+	 * Method which controls the gameturns. Starts the turn in gamelogic, and is controlled by network
+	 * @return always returns true, will only return true when the entire turn is finished though.
+	 *
+	 */
+	public boolean turn(){
+		System.err.println("Start of turn");
+		world.startTurn();
+
+		//When this loop breaks, turn is over
+
 		return true; //All the networking data has now been sent, the ClientThread has given the OK, end the turn properly.
 	}
 
